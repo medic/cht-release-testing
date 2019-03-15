0d335076b2cb77920b78d6c572aabd1d62e9a232
Nick <emailfornc@gmail.com>
2019-03-15 14:48:34 -0400

    wrote medic-conf script to seed data

---
+++ data_seed.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+echo -n Instance Name: 
+read -s instance_name
+echo
+
+if [ $instance_name = "local" ]
+then
+  instance_name="--local"
+else
+  instance_name="--instance $instance_name"
+fi  
+  
+
+if ! [ -e ".gdrive.secrets.json" ]
+then 
+  echo "gdrive secrets file does not exist. It is required to get csvs for seeding"
+fi
+
+if ! [ -e "csvs-on-google-drive.json" ]
+then 
+  echo "csvs-on-google-drive.json file does not exist. It is required to get csvs for seeding"
+fi
+
+medic-conf fetch-csvs-from-google-drive
+medic-conf csv-to-docs
+medic-conf $instance_name upload-docs
+medic-conf $instance_name  create-users
